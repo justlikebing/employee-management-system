f8a3cbbf2da83b9216bf45c73721b75bd6a41261
Kumar Dev <sdevssingh0012@gmail.com>
2024-03-07 23:48:34 +0530

    jwt token

---
+++ src/main/java/com/example/jwtRefresh/JwtService.java
@@ -0,0 +1,98 @@
+package com.example.jwtRefresh;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.apache.catalina.Authenticator;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JwtService {
+
+
+    @PostMapping("/login")
+    public JwtResponseDTO authenticateAndGetToken(@RequestBody AuthRequestDTO authRequestDTO) {
+        try {
+
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequestDTO.getUsername(), authRequestDTO.getPassword()));
+            RefreshToken refreshToken = refreshTokenService.createRefreshToken(authRequestDTO.getUsername());
+            return JwtResponseDTO.builder()
+                    .accessToken(jwtService.generateToken(authRequestDTO.getUsername()))
+                    .token(refreshToken.getToken())
+                    .build();
+        } catch (AuthenticationException e) {
+            throw new UsernameNotFoundException("Invalid username or password");
+        }
+    }
+
+    public static final String SECRET = "357638792F423F4428472B4B6250655368566D597133743677397A2443264629";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+
+
+    public String GenerateToken(String username){
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username);
+    }
+
+
+
+    private String createToken(Map<String, Object> claims, String username) {
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*1))
+                .signWith(SignatureAlgorithm.HS256, getSignKey()).compact();
+    }
+
+    private SecretKeySpec getSignKey() {
+        byte[] decodedKey = Base64.getDecoder().decode(SECRET);
+        return new SecretKeySpec(decodedKey, "HmacSHA256");
+    }
+}
